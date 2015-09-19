c256094253a9d9f75794589fc7bcae0e1a938e31
John Gasper <jgasper@unicon.net>
2015-09-18 23:36:54 -0700

    clean-up

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # base-jetty
 
 ```
-docker build --tag=base-jetty .
+docker build --tag=base-jetty github.com/anan-shibboleth/base-jetty.git
 ```
--- container-scripts/run-jetty.sh
+++ container-scripts/run-jetty.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 set -x
 
-export JAVA_HOME=/opt/jre1.8.0_60
-export JETTY_HOME=/opt/jetty/
-export JETTY_BASE=/opt/iam-jetty-base/
-export PATH=$PATH:$JAVA_HOME/bin
+export JAVA_HOME=/opt/jre1.8.0_60/bin
+export PATH=$PATH:$JAVA_HOME
 
-sed -i "s/^-Xmx.*$/-Xmx$JETTY_MAX_HEAP/g" /opt/iam-jetty-base/start.ini
+sed -i "s/^-Xmx.*$/-Xmx$JETTY_MAX_HEAP/g" /opt/shib-jetty-base/start.ini
 
 /etc/init.d/jetty run
